8a7544f6249f3906981a5af731632b25c48f24fb
franklioxygen <frank.li.oxygen@gmail.com>
2022-06-17 16:17:45 -0400

    sort record

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,17 +4,40 @@ import { Link } from "react-router-dom";
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      records: this.props.records,
+      lastSort: null,
+    };
   }
 
-  genTime = timestamp => {
-    let date =  new Date(timestamp);
-    let y = date.getFullYear();
-    let m = date.getMonth();
-    let d = date.getDate();
-    let hrs = `0${date.getHours()}`.slice(-2);
-    let min = `0${date.getMinutes()}`.slice(-2);
-    return `${y}-${m}-${d}  ${hrs}:${min}`;
+  sort = column => {
+    if(this.state.lastSort === column) {
+      // reverse if same column sorted
+      this.setState({
+        records: this.state.records.reverse(),
+    });
+    } else {
+    // sort by column
+    this.setState({
+      records: this.state.records.sort((a, b) => {
+        return a.properties[column] - b.properties[column]
+      })
+    });
+    this.setState({lastSort: column});
+    }
   }
+
+formatDate = (timestamp) => {
+    let date =  new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes.toString().padStart(2, '0');
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return `${date.toString().substring(4,15)}, ${strTime}`;
+}
 
   render() {
     return (
@@ -24,19 +47,16 @@ class Home extends React.Component {
         <tbody>
         <tr>
           <td>Title</td>
-          <td>Magnitude</td>
-          <td>Time</td>
+          <td onClick={()=>this.sort('mag')}>Magnitude</td>
+          <td onClick={()=>this.sort('time')}>Time</td>
         </tr>
-      {this.props.records.map((record, index) => {
+      {this.state.records.map((record, index) => {
               return (
                   <tr key={index}>
-                    {/* <td> <Link to={{
-                        pathname: "detail",
-                        search: "?id="+record.id,
-                      }}>{record.properties.place}</Link></td> */}
                     <td> <Link to={`/detail/${record.id}`}>{record.properties.place}</Link></td>
                     <td>{record.properties.mag}</td>
-                    <td>{this.genTime(record.properties.time)}</td>
+                    {/* <td>{this.genTime(record.properties.time)}</td> */}
+                    <td>{this.formatDate(record.properties.time)}</td>
                   </tr>
               );
             })}
